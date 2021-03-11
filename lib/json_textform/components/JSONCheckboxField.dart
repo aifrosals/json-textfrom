4e2e93c5b7512b10cc5dff7a20f529b021453925
aifrosals <42416508+aifrosals@users.noreply.github.com>
2021-03-11 11:42:21 +0500

    Update JSONCheckboxField.dart
    new commit

--- lib/json_textform/components/JSONCheckboxField.dart
+++ lib/json_textform/components/JSONCheckboxField.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../models/Schema.dart';
+import 'dart:core';
 
-typedef void OnChange(bool value);
+typedef void OnChange(List<dynamic> value);
 
 class JSONCheckboxField extends StatelessWidget {
   final Schema schema;
@@ -18,13 +19,35 @@ class JSONCheckboxField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(
-      value: schema.value ?? schema?.extra?.defaultValue ?? false,
-      onChanged: (v) {
-        onSaved(v);
-      },
-      title: Text("${schema.label}"),
-      subtitle: Text("${schema?.extra?.helpText ?? ""}"),
+
+ return
+    Column(
+      mainAxisSize: MainAxisSize.min,
+     children :
+        <Widget>[
+           Align(
+             alignment: Alignment.topCenter,
+           child: Text(schema.label),
+           ),
+        ListView.builder(
+        shrinkWrap: true,
+        itemCount: schema.options.length,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return CheckboxListTile(
+            value: (schema.value == null) ? false : schema.value.asMap().containsKey(index) ? schema.value[index]["optionValue"] ? true : false : false,
+            onChanged: (v) {
+              print('this is v');
+              schema.options[index]['optionValue'] = v;
+              // List<dynamic> list = data;
+
+              onSaved(schema.options);
+            },
+            title: Text("${schema.options[index]['optionLabel']}"),
+          );
+        }),
+   ],
     );
+
   }
 }
